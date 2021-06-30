643c876051198ef2a6cae85d7edc60a1c6bbe296
arush <arushp59@gmail.com>
2021-07-01 00:22:14 +0530

    Hybrid Inheritance

---
+++ Hybrid.java
@@ -0,0 +1,3 @@
+Hybrid Inheritance : It is the combination of Single Inheritance and Multiple Inheritance so Java does not support
+		     Hybrid Inheritance with classes. In Java we can achieve Hybrid Inheritance only through 
+		     Interfaces.
